c8cad972511c36b75078cf2fa8ab71b7cd153910
John Kindem <461425614@qq.com>
2018-06-25 15:33:50 +0800

    优化服务器结构

---
+++ config.js
@@ -0,0 +1,13 @@
+// 数据库连接信息
+module.exports.dbConnectionInfo = {
+    host: '127.0.0.1',
+    port: '3306',
+    user: 'kindem',
+    password: '123456',
+    database: 'student_info_manager',
+    connectTimeout: 1000
+};
+
+// 管理员密码hash
+module.exports.adminPasswordHash = '8c6976e5b5410415bde908bd4dee15dfb167a9c873fc4bb8a81f6f2ab448a918';
+
